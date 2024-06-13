2507f8bc766d1acb10e8681cd811c959c11f09cc
VictorBo <hi@vtrbo.cn>
2024-06-13 23:13:43 +0800

    perf: 优化创建子模块的便捷性

--- scripts/create-subpackage.ts
+++ scripts/create-subpackage.ts
@@ -1,13 +1,18 @@
 import process from 'node:process'
 import fs from 'node:fs'
 import { basename, resolve } from 'node:path'
-import { resolveRealPath } from './utils'
+import { fileURLToPath } from 'node:url'
+import ora from 'ora'
+import MagicString from 'magic-string'
+import { resolveRealPath, runCommand } from './utils'
 
 const [, , ...args] = process.argv
 
+const subpackageNamePrefix = '@xparcai-utils/'
 const packagesDirPath = resolveRealPath('../packages')
 const templateDirPath = resolveRealPath('../template')
 
+// 复制模板文件
 function copySubpackageTemplate({
   templateDirPath,
   subpackageDirPath,
@@ -18,8 +23,8 @@ function copySubpackageTemplate({
   subpackageName: string
 }) {
   const stats = fs.statSync(templateDirPath)
+  // 创建文件夹
   if (stats.isDirectory()) {
-    // 创建文件夹
     fs.mkdirSync(subpackageDirPath, { recursive: true })
     for (const file of fs.readdirSync(templateDirPath)) {
       copySubpackageTemplate({
@@ -50,7 +55,6 @@ function copySubpackageTemplate({
   // 改rollup.config.ts
   if (basename(templateDirPath) === 'rollup.config.ts') {
     let rollupConfig = fs.readFileSync(templateDirPath, 'utf-8')
-    console.log(rollupConfig)
     rollupConfig = rollupConfig.replace('../scripts/rollup-config', '../../scripts/rollup-config')
     fs.writeFileSync(subpackageDirPath, rollupConfig)
     return
@@ -59,23 +63,92 @@ function copySubpackageTemplate({
   fs.copyFileSync(templateDirPath, subpackageDirPath)
 }
 
+// 排序package.json dependencies
+function sortPackageDependencies(obj: Record<string, string>) {
+  const keys = Object.keys(obj).sort()
+  return keys.reduce((acc: Record<string, string>, key) => {
+    acc[key] = obj[key]
+    return acc
+  }, {})
+}
+
+// 添加到core包依赖
+function insertCorePackageDependencies(subpackageNameFull: string) {
+  const coreDirPath = resolveRealPath(`${packagesDirPath}/core/package.json`)
+  const pkg = JSON.parse(fs.readFileSync(coreDirPath, 'utf-8'))
+  pkg.dependencies = sortPackageDependencies({
+    ...pkg.dependencies,
+    [subpackageNameFull]: 'workspace:*',
+  })
+  fs.writeFileSync(coreDirPath, `${JSON.stringify(pkg, null, 2)}\n`)
+}
+
+// 添加到core包主入口导出
+function insertCorePackageExport(subpackageNameFull: string) {
+  const coreDirPath = resolveRealPath(`${packagesDirPath}/core/index.ts`)
+  const coreExport = fs.readFileSync(coreDirPath, 'utf-8')
+  const mst = new MagicString(coreExport)
+  mst.append(`export * from '${subpackageNameFull}'`)
+  fs.writeFileSync(coreDirPath, `${mst.toString()}\n`)
+}
+
+// 安装依赖
+function installDependencies() {
+  const dirname = fileURLToPath(new URL('../', import.meta.url))
+  return runCommand(`pnpm install`, dirname)
+}
+
+// 转换为_-连接的字符
+function toLinesCase(str: string, isUnder: boolean = false): string {
+  return str
+    .replace(/^[-|_]+|[-|_]+$/g, '')
+    .replace(/^[A-Z]/g, key => key.toLowerCase())
+    .replace(/[A-Z]/g, key => `${isUnder ? '_' : '-'}${key.toLowerCase()}`)
+}
+
+// 创建子包
 function createSubpackage() {
-  const subpackageName = args[0]
+  let subpackageName = args[0]
   if (!subpackageName) {
     return console.log('未提供子包的名称')
   }
 
-  // TOTO 这里需要规范一下子包的类型，短横线连接
-  const subpackageDirPath = resolveRealPath(`${packagesDirPath}/${subpackageName}`)
+  // 转换为短横线连接
+  subpackageName = toLinesCase(subpackageName)
 
+  // 子包全名
+  const subpackageNameFull = `${subpackageNamePrefix}${subpackageName}`
+
+  // 校验子包是否存在
+  const subpackageDirPath = resolveRealPath(`${packagesDirPath}/${subpackageName}`)
   if (fs.existsSync(subpackageDirPath)) {
-    return console.log(`子包[${subpackageName}]已存在`)
+    return console.log(`子包[${subpackageNameFull}]已存在`)
   }
 
-  // TODO 这里思考到底是以一个模块为蓝本还是设置好一套固定的模板
-  copySubpackageTemplate({ templateDirPath, subpackageDirPath, subpackageName })
+  const spinnersPrefixText = '[xparcai-utils]: '
+  const spinners = ora()
 
-  console.log(`创建[${subpackageName}]子包成功！`)
+  spinners.text = `${spinnersPrefixText}创建[${subpackageNameFull}]子包中...`
+  spinners.start()
+
+  // 复制模板文件
+  copySubpackageTemplate({ templateDirPath, subpackageDirPath, subpackageName })
+  // 添加到core的生产依赖中
+  insertCorePackageDependencies(subpackageNameFull)
+  // 添加到core包主入口导出
+  insertCorePackageExport(subpackageNameFull)
+
+  spinners.succeed(`${spinnersPrefixText}创建[${subpackageNameFull}]子包成功`)
+
+  spinners.text = `${spinnersPrefixText}依赖安装中...`
+  spinners.start()
+
+  // 安装依赖
+  installDependencies().then(() => {
+    spinners.succeed(`${spinnersPrefixText}安装依赖成功`)
+  }).catch(() => {
+    spinners.fail(`${spinnersPrefixText}安装依赖失败`)
+  })
 }
 
 createSubpackage()
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,8 +1,45 @@
+import type { ProcessEnvOptions } from 'node:child_process'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import type { ExecOptions } from 'shelljs'
+import shell from 'shelljs'
 
 export function resolveRealPath(path: string) {
   const fileName = fileURLToPath(import.meta.url)
   const dirName = dirname(fileName)
   return resolve(dirName, path)
+}
+
+export function runCommand(command: string, dir: ProcessEnvOptions['cwd'], userOptions: ExecOptions = {}) {
+  return new Promise((resolve, reject) => {
+    try {
+      shell.exec(
+        command,
+        {
+          cwd: dir,
+          shell: true,
+          encoding: 'GBK',
+          async: true,
+          silent: userOptions?.silent ?? true,
+          ...userOptions,
+        } as any,
+        (code, output, err) => {
+          if (code === 0) {
+            resolve(true)
+          }
+          else if (err) {
+            reject(err)
+          }
+
+          const outputStr = output.toString()
+          if (outputStr && !(userOptions?.silent ?? true)) {
+            console.log(outputStr)
+          }
+        },
+      )
+    }
+    catch (e) {
+      reject(e.message)
+    }
+  })
 }
