2b0146403fbdb350eb8a57ae6a1d39d4bc155254
VictorBo <hi@vtrbo.cn>
2024-06-17 02:44:57 +0800

    feat: 新增isBoolean方法

---
+++ packages/is/__test__/isBoolean.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+import { isBoolean } from '../index'
+
+describe('@xparcai-utils/is', () => {
+  it('isBoolean', () => {
+    expect(isBoolean(true)).toBe(true)
+    expect(isBoolean(false)).toBe(true)
+    expect(isBoolean(!!0)).toBe(true)
+    expect(isBoolean(!!1)).toBe(true)
+    expect(isBoolean(!undefined)).toBe(true)
+    expect(isBoolean(!null)).toBe(true)
+  })
+})
