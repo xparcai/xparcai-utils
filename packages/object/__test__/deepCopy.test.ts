a7f014e51c5ac0253d19572e74bf56352c180e02
VictorBo <hi@vtrbo.cn>
2024-06-22 00:10:01 +0800

    feat: 调整deepCopy测试用例

--- packages/object/__test__/deepCopy.test.ts
+++ packages/object/__test__/deepCopy.test.ts
@@ -4,50 +4,55 @@ import { deepCopy } from '..'
 describe('@xparcai-utils/object', () => {
   it('deepCopy: 复制基本类型', () => {
     let data: any = 666
-    expect(deepCopy(data)).toStrictEqual(data)
+    expect(deepCopy(data)).toBe(data)
     data = 'xparcai'
-    expect(deepCopy(data)).toStrictEqual(data)
+    expect(deepCopy(data)).toBe(data)
     data = false
-    expect(deepCopy(data)).toStrictEqual(data)
-    // ......
+    expect(deepCopy(data)).toBe(data)
+    data = null
+    expect(deepCopy(data)).toBeNull()
+    data = undefined
+    expect(deepCopy(data)).toBeUndefined()
   })
 
   it('deepCopy: 复制数组', () => {
     const data: any = ['0.0.1', '0.0.2']
-    expect(deepCopy(data)).toStrictEqual(data)
+    expect(deepCopy(data)).toEqual(data)
+    expect(deepCopy(data)).not.toBe(data)
     data.push([...data])
-    expect(deepCopy(data)).toStrictEqual(data)
-    data.push({
-      name: 'xparcai',
-      data: new Date(),
-      regexp: new RegExp('xparcai'),
-      version: 1,
-      changeLog: ['0.0.1', '0.0.2'],
-    })
-    // ......
+    expect(deepCopy(data)).toEqual(data)
+    expect(deepCopy(data)).not.toBe(data)
   })
 
   it('deepCopy: 复制对象', () => {
     const data: any = {
       name: 'xparcai',
-      data: new Date(),
       regexp: new RegExp('xparcai'),
       version: 1,
       changeLog: ['0.0.1', '0.0.2'],
+      obj: {
+        a: 1,
+        b: [1, 2, 3],
+        c: {
+          b: 1,
+        },
+      },
     }
-    expect(deepCopy(data)).toStrictEqual(data)
-    data.inner = { ...data }
-    expect(deepCopy(data)).toStrictEqual(data)
-    // ......
+    expect(deepCopy(data)).toEqual(data)
+    expect(deepCopy(data)).not.toBe(data)
+    expect(deepCopy(data).changeLog).not.toBe(data.changeLog)
+    expect(deepCopy(data).regexp).not.toBe(data.regexp)
+    expect(deepCopy(data).obj).not.toBe(data.obj)
+    expect(deepCopy(data).obj.c).not.toBe(data.obj.c)
   })
 
   it('deepCopy: 复制函数', () => {
     let data: any = function () {
       return ''
     }
-    expect(deepCopy(data)).toStrictEqual(data)
+    expect(deepCopy(data)).toBe(data)
     data = () => {}
-    expect(deepCopy(data)).toStrictEqual(data)
+    expect(deepCopy(data)).toBe(data)
     // ......
   })
 })
--- packages/object/src/deepCopy.ts
+++ packages/object/src/deepCopy.ts
@@ -24,37 +24,32 @@ export function deepCopy<T>(data: T, hash: any = new WeakMap()): T {
   if (isRegExp(data)) {
     return new RegExp(data) as T
   }
-  let _desc: any
-  let _data: any
+  // let _desc: any
+  // let _data: any
   if (isArray(data) || isObject(data)) {
     // 如果循环引用,就用 weakMap 来解决
     if (hash.has(data)) {
       return hash.get(data)
     }
-    // 获取对象所有自身属性的描述
-    _desc = Object.getOwnPropertyDescriptors(data)
-    // 遍历传入参数所有键的特性
-    _data = Object.create(Object.getPrototypeOf(data), _desc)
-    hash.set(data, _data)
+    // // 获取对象所有自身属性的描述
+    // _desc = Object.getOwnPropertyDescriptors(data)
+    // // 遍历传入参数所有键的特性
+    // _data = Object.create(Object.getPrototypeOf(data), _desc)
+    // hash.set(data, _data)
   }
   // 数组对象返回一个深拷贝的数组对象
   if (isArray(data)) {
+    const _arr: any = []
     for (const key in data)
-      _data.push(deepCopy(data[key], hash))
-    return _data
+      _arr.push(deepCopy(data[key], hash))
+    return _arr
   }
   // 对象返回深拷贝的对象
   if (isObject(data)) {
-    for (const key of Reflect.ownKeys(data as object)) {
-      const dataKeyValue = (data as Record<string | symbol, any>)[key]
-      if (typeof dataKeyValue === 'object' && dataKeyValue !== null) {
-        _data[key] = deepCopy(dataKeyValue, hash)
-      }
-      else {
-        _data[key] = dataKeyValue
-      }
-    }
-    return _data
+    const _obj: any = {}
+    for (const key in data)
+      _obj[key] = deepCopy(data[key], hash)
+    return _obj
   }
   // 基本数据类型
   return data
