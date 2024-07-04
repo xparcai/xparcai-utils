a4d8fa2a2bcf432ccc4e3145c01ee7a76cf38414
VictorBo <hi@vtrbo.cn>
2024-07-04 15:00:42 +0800

    feat: 增加isEmptyString的单测，但未通过单测

--- packages/is/__test__/isEmptyString.test.ts
+++ packages/is/__test__/isEmptyString.test.ts
@@ -3,6 +3,8 @@ import { isEmptyString } from '../src/isEmptyString'
 
 describe('@xparcai-utils/is', () => {
   it('isEmptyString', () => {
-    expect(isEmptyString()).toBe(undefined)
+    expect(isEmptyString(1)).toBe(false)
+    // 这里的单测没有通过，预期应该是true，但得到了false
+    expect(isEmptyString('')).toBe(true)
   })
 })
